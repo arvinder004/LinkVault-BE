04a179356db8d6cd7ebdbd224a48105b1fb328f6
arvinder004 <asdhoul004@gmail.com>
2025-10-06 08:37:28 +0530

    added new frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
